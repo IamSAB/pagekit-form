b47856cc53696eff5de91b2aa4af6232105fae53
IamSAB <2011pbosi@gmail.com>
2018-12-20 11:46:42 +0100

    Test PHP Html Parse instead Simple One

--- src/Plugin/FormPlugin.php
+++ src/Plugin/FormPlugin.php
@@ -6,8 +6,8 @@ namespace SAB\Form\Plugin;
 use Pagekit\Application as App;
 use Pagekit\Content\Event\ContentEvent;
 use Pagekit\Event\EventSubscriberInterface;
-use Sunra\PhpSimple\HtmlDomParser;
 use Pagekit\Util\Arr;
+use PHPHtmlParser\Dom;
 
 class FormPlugin implements EventSubscriberInterface
 {
@@ -19,19 +19,20 @@ class FormPlugin implements EventSubscriberInterface
      */
     public function onContentPlugins(ContentEvent $event)
     {
-        $content = HtmlDomParser::str_get_html($event->getContent());
+        $dom = new Dom;
+        $dom->load($event->getContent());
 
-        foreach ($content->find('form[to][subject]') as $index => $form) { // find forms with to attribute
+        foreach ($dom->find('form[to][subject]') as $i => &$form) { // find forms with to attribute
 
-            $id = 'form'.($index+1);
+            $id = 'form'.($i+1);
 
-            $attrs = $form->getAllAttributes();
+            $attrs = $form->getAttributes();
 
             $adresses = Arr::extract($attrs,['to','cc','bbc','replyto']);
             $mail = Arr::extract($attrs, ['title', 'subject', 'desc', 'priority']);
             $values = [];
 
-            foreach($form->find('input[name], select[name], textarea[name]') as $index => $input) {
+            foreach($form->find('input[name], select[name], textarea[name]') as $j => $input) {
 
                 if ($input->tag == 'select' && $input->multiple) { // TODO currently setting multiple times default
                     $values[$input->name] = [];
@@ -65,23 +66,31 @@ class FormPlugin implements EventSubscriberInterface
             }
 
             // submission handling
-            $form->find('input[type=submit]',0)->setAttribute('v-show','status == 0');
-
-            if ($success = $form->find('*[success]',0)) {
-                $success->setAttribute('v-show', 'status == 2');
-            }
-            if ($error = $form->find('*[error]',0)) {
-                $error->setAttribute('v-show', 'status == 3');
+            if($form->find('input[type=submit]')->count()) {
+                $form->find('input[type=submit]',0)->setAttribute('v-show','status == 0');
             }
 
-            $html = HtmlDomParser::file_get_html(App::locator()->get('form:views/form.html'));
-            $html->find('div', 0)->setAttribute('id', $id);
-            $html->find('form',0)->setAttribute('class', $form->getAttribute('class'));
-            $html->find('fieldset',0)->innertext = $form->innertext;
-            $form->outertext = $html;
+            if ($form->find('*[success]')->count()) {
+                $form->find('*[success]',0)->setAttribute('v-show', 'status == 2');
+            }
+            if ($form->find('*[error]')->count()) {
+                $form->find('*[error]')->setAttribute('v-show', 'status == 3');
+            }
+            $tmpl = new Dom;
+            $tmpl->load(App::locator()->get('form:views/form.html'));
+
+            $tmpl->find('div', 0)->setAttribute('id', $id);
+            $tmpl->find('form',0)->setAttribute('class', $form->getAttribute('class'));
+            $tmpl->find('fieldset',0)->addChild($form);
+            var_dump($tmpl->root->children[$tmpl->root->getChildren()[0]->id()]['prev']);
+            // var_dump($form->id());
+            // $form->getParent()->replaceChild($form->id(), $tmpl->root);
+            // var_dump($form->getParent()->getTag()->name());
+            // var_dump($form->countChildren());
+            // $form->getChild($tmpl->root->id());
         }
 
-        $event->setContent($content->save());
+        $event->setContent((string) $dom);
     }
 
     /**
